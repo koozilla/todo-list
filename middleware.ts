fc9677bdde0aa997ecb6c35dc3a0d96fd98cb6c1
koozilla <nathan.lee.fin@gmail.com>
2025-08-25 13:25:40 -0700

    Fix middleware interfering with OAuth callback - Exclude auth/callback route from middleware processing - This should resolve OAuth authentication issues in production

--- middleware.ts
+++ middleware.ts
@@ -55,7 +55,8 @@ export const config = {
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
      * - public folder
+     * - auth/callback (OAuth callback route)
      */
-    '/((?!_next/static|_next/image|favicon.ico|public).*)',
+    '/((?!_next/static|_next/image|favicon.ico|public|auth/callback).*)',
   ],
 }
